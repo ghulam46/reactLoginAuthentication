d2c19dc08511b7112c5dd5a47351d65d44c205e4
ghulam46 <ghulam.yanuar01@gmail.com>
2023-02-23 20:14:18 +0700

    implement route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Preferences from './components/Preferences';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <Router>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/preferences' element={<Preferences/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
