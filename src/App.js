0b5b7ef46213ec26f49e239133118b18f55f30cc
ghulam46 <ghulam.yanuar01@gmail.com>
2023-02-23 20:39:46 +0700

    add component dashboard, preferences & add token for login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 // import logo from './logo.svg';
-import './App.css';
+import '../src/component/Wrapper.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import Preferences from './components/Preferences';
+import Dashboard from '../src/component/Dashboard';
+import Preferences from '../src/component/Preferences';
+import { useState } from 'react';
+import Login from './component/Login';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
