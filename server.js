40638dfc142793da00eeef7c06ab6dda3b75960d
ghulam46 <ghulam.yanuar01@gmail.com>
2023-02-24 14:41:48 +0700

    add server for api

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
+
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'test123'
+  });
+});
+
+// app.use('/register', (req, res) => {
+//     res.send({
+//         nama: 'Ghulam',
+//         umur: 22,
+//         alamat: 'Jalan Jati',
+//     })
+// });
+
+app.listen(3001, () => console.log('API is running on http://localhost:3001/login'));
